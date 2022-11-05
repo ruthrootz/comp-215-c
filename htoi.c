147f144ef67789490e50c043adbc1ef8bff1c795
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2022-11-05 19:45:35 +0000

    most tests working

--- htoi.c
+++ htoi.c
@@ -5,37 +5,40 @@
 */
 
 #include <stdio.h>
+#include <math.h>
 #include "htoi.h"
 
 int main(void) {
-  // TESTS:
-  // 0X
-  // 0x
-  // lowercase a-f
-  // uppercase A-F
-  // zero
-  // MAX
-  return 0;
+    printf("%d\n", htoi("0X1"));
+    printf("%d\n", htoi("0x1"));
+    printf("%d\n", htoi("97"));
+    printf("%d\n", htoi("0X7B"));
+    printf("%d\n", htoi("0Xaa")); // BUG
+    printf("%d\n", htoi("AA"));
+    printf("%d\n", htoi("ff")); // BUG
+    printf("%d\n", htoi("FF"));
+    printf("%d\n", htoi("0x12345"));
+    printf("%d\n", htoi("FFff"));
+    printf("%d\n", htoi("0X0"));
+    
+    return 0;
 }
 
 int htoi(char hexString[])
 {
   int decimalValue = 0;
-  int placeValue = 1;
-
-  for (int i = strlen(hexString); i > 0; i--)
-  {
-    if (hexString[i] == '\0')
+  int placeValue = 0;
+    int startingIndex = strlen(hexString) - 1;
+    int endingIndex = 0;
+    if (hexString[1] == 'x' || hexString[1] == 'X')
     {
-      continue;
-    }
-    else if (hexString[i] == 'x' || hexString[i] == 'X')
-    {
-      break;
+        endingIndex = 2;
     }
 
-    int convertedDigit = hexToDecimal(hexString[i], placeValue);
-    placeValue *= 16;
+  for (int i = startingIndex; i >= endingIndex; i--)
+      {
+    int convertedDigit = hexToDecimal(hexString[i], pow(16, placeValue));
+    placeValue++;
     decimalValue += convertedDigit;
   }
 
@@ -45,20 +48,21 @@ int htoi(char hexString[])
 int hexToDecimal(char hexDigit, int placeValue)
 {
   int intValueOfChar = 0;
+    
   if (hexDigit >= 'a' && hexDigit <= 'f')
   {
-    intValueOfChar = hexDigit - 87;
+    intValueOfChar = hexDigit - 107;
   }
   else if (hexDigit >= 'A' && hexDigit <= 'F')
   {
     intValueOfChar = hexDigit - 55;
   }
-  else if (hexDigit >= 0 && hexDigit <= 9)
+  else if (hexDigit >= '0' && hexDigit <= '9')
   {
     intValueOfChar = hexDigit - 48;
   }
 
-  int decimalValue = hexDigit * placeValue;
+  int decimalValue = intValueOfChar * placeValue;
 
   return decimalValue;
 }
