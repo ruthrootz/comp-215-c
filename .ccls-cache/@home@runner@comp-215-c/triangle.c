1802fc6fdb03972f77f478e8fcf3b0f4faaf2ee1
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2022-12-14 00:42:35 +0000

    cleanup

---
+++ .ccls-cache/@home@runner@comp-215-c/triangle.c
@@ -0,0 +1,45 @@
+/*
+    name: Ruth Oldja
+    date: 11/1/2022
+    purpose: output a triangle of n rows, where n is user input, an integer between 1 and 20 (inclusive)
+*/
+#include <stdio.h>
+
+int main(void) {
+    // get user input
+    int rows = 0;
+    printf("Enter a number [1, 20]: ");
+    scanf("%d", &rows);
+    
+    // validate user input
+    while (!(rows <= 20 && rows >= 1)) {
+        printf("Enter a number [1, 20]: ");
+        scanf("%d", &rows);
+    }
+    
+    // figure out how long each line will be
+    int columns = 2 * rows - 1;
+    
+    for (int i = 0; i < rows; i++) {
+        // if this is the last row:
+        if (i == rows - 1) {
+            for (int j = 0; j < columns; j++) {
+                printf("*");
+            }
+        }
+        // otherwise:
+        else {
+            for (int j = 1; j <= columns; j++) {
+                if (j == rows - i || j == columns - (rows - i - 1)) {
+                    printf("*");
+                } else {
+                    printf(" ");
+                }
+            }
+        }
+        
+        printf("\n");
+    }
+
+    return 0;
+}
--- triangle.c
+++ triangle.c
@@ -6,40 +6,40 @@
 // #include <stdio.h>
 
 // int main(void) {
-//   // get user input
-//   int rows = 0;
-//   printf("Enter a number [1, 20]: ");
-//   scanf("%d", &rows);
-
-//   // validate user input
-//   while (!(rows <= 20 && rows >= 1)) {
+//     // get user input
+//     int rows = 0;
 //     printf("Enter a number [1, 20]: ");
 //     scanf("%d", &rows);
-//   }
-
-//   // figure out how long each line will be
-//   int columns = 2 * rows - 1;
-
-//   for (int i = 0; i < rows; i++) {
-//     // if this is the last row:
-//     if (i == rows - 1) {
-//       for (int j = 0; j < columns; j++) {
-//         printf("*");
-//       }
+    
+//     // validate user input
+//     while (!(rows <= 20 && rows >= 1)) {
+//         printf("Enter a number [1, 20]: ");
+//         scanf("%d", &rows);
 //     }
-//     // otherwise:
-//     else {
-//       for (int j = 1; j <= columns; j++) {
-//         if (j == rows - i || j == columns - (rows - i - 1)) {
-//           printf("*");
-//         } else {
-//           printf(" ");
+    
+//     // figure out how long each line will be
+//     int columns = 2 * rows - 1;
+    
+//     for (int i = 0; i < rows; i++) {
+//         // if this is the last row:
+//         if (i == rows - 1) {
+//             for (int j = 0; j < columns; j++) {
+//                 printf("*");
+//             }
 //         }
-//       }
+//         // otherwise:
+//         else {
+//             for (int j = 1; j <= columns; j++) {
+//                 if (j == rows - i || j == columns - (rows - i - 1)) {
+//                     printf("*");
+//                 } else {
+//                     printf(" ");
+//                 }
+//             }
+//         }
+        
+//         printf("\n");
 //     }
 
-//     printf("\n");
-//   }
-
-//   return 0;
+//     return 0;
 // }
